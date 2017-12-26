045806537e414bf47979d74a6be5b8571966b871
Harm te Hennepe <harm@tehennepe.org>
2017-12-26 21:56:00 +0100

    Koning Harm

--- caps2key.c
+++ caps2key.c
@@ -1,18 +1,13 @@
-// compile:
-//     gcc caps2esc.c -o caps2esc -I/usr/include/libevdev-1.0 -levdev -ludev
-// run:
-//     sudo nice -n -20 ./caps2esc >caps2esc.log 2>caps2esc.err &
-
-#include <stdio.h>
 #include <errno.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <fcntl.h>
 #include <signal.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include <libudev.h>
 #include <sys/select.h>
@@ -22,12 +17,18 @@
 
 // clang-format off
 const struct input_event
-esc_down      = {.type = EV_KEY, .code = KEY_ESC,      .value = 1},
-ctrl_down     = {.type = EV_KEY, .code = KEY_LEFTCTRL, .value = 1},
-capslock_down = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 1},
-esc_up        = {.type = EV_KEY, .code = KEY_ESC,      .value = 0},
-ctrl_up       = {.type = EV_KEY, .code = KEY_LEFTCTRL, .value = 0},
-capslock_up   = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 0};
+esc_down      = {.type = EV_KEY, .code = KEY_ESC,       .value = 1},
+lctrl_down    = {.type = EV_KEY, .code = KEY_LEFTCTRL,  .value = 1},
+rctrl_down    = {.type = EV_KEY, .code = KEY_RIGHTCTRL, .value = 1},
+capslock_down = {.type = EV_KEY, .code = KEY_CAPSLOCK,  .value = 1},
+ralt_down     = {.type = EV_KEY, .code = KEY_RIGHTALT,  .value = 1},
+lalt_down     = {.type = EV_KEY, .code = KEY_LEFTALT,   .value = 1},
+esc_up        = {.type = EV_KEY, .code = KEY_ESC,       .value = 0},
+lctrl_up      = {.type = EV_KEY, .code = KEY_LEFTCTRL,  .value = 0},
+rctrl_up      = {.type = EV_KEY, .code = KEY_RIGHTCTRL, .value = 0},
+capslock_up   = {.type = EV_KEY, .code = KEY_CAPSLOCK,  .value = 0},
+ralt_up       = {.type = EV_KEY, .code = KEY_RIGHTALT,  .value = 0},
+lalt_up       = {.type = EV_KEY, .code = KEY_LEFTALT,   .value = 0};
 // clang-format on
 
 int equal(const struct input_event *first, const struct input_event *second) {
@@ -35,65 +36,90 @@ int equal(const struct input_event *first, const struct input_event *second) {
            first->value == second->value;
 }
 
-int eventmap(const struct input_event *input, struct input_event output[]) {
-    static int capslock_is_down = 0, esc_give_up = 0;
+enum state_e { CAPS_UP, CAPS_DOWN, CAPS_DOWN_MOD_PRESSED, CAPS_DOWN_MAYBE_CHANGE };
 
+struct state_s {
+    enum state_e fsm;
+    const struct input_event *key_up;
+    const struct input_event *key_down;
+    struct input_event last_input;
+};
+
+int eventmap(const struct input_event *input, struct input_event output[], struct state_s *state) {
     if (input->type != EV_KEY) {
         output[0] = *input;
         return 1;
     }
 
-    if (capslock_is_down) {
-        if (equal(input, &capslock_down) || input->code == KEY_LEFTCTRL) {
-            return 0;
-        }
-        if (equal(input, &capslock_up)) {
-            capslock_is_down = 0;
-            if (esc_give_up) {
-                esc_give_up = 0;
-                output[0]   = ctrl_up;
-                return 1;
-            }
-            output[0] = esc_down;
-            output[1] = esc_up;
-            return 2;
-        }
+    int k = 0;
 
-        int k = 0;
-
-        if (!esc_give_up && input->value) {
-            esc_give_up = 1;
-            output[k++] = ctrl_down;
-        }
-
-        if (equal(input, &esc_down))
-            output[k++] = capslock_down;
-        else if (equal(input, &esc_up))
-            output[k++] = capslock_up;
+    switch (state->fsm) {
+    case CAPS_UP:
+        if (equal(input, &capslock_down))
+            state->fsm = CAPS_DOWN;
         else
             output[k++] = *input;
-
-        return k;
+        break;
+    case CAPS_DOWN:
+        if (equal(input, &capslock_up)) {
+            output[k++] = esc_down;
+            output[k++] = esc_up;
+            state->fsm = CAPS_UP;
+        } else if (equal(input, &lctrl_down) || equal(input, &rctrl_down) ||
+                   equal(input, &lalt_down)  || equal(input, &ralt_down)) {
+            state->last_input = *input;
+            state->fsm = CAPS_DOWN_MAYBE_CHANGE;
+        } else {
+            output[k++] = *state->key_down;
+            output[k++] = *input;
+            state->fsm = CAPS_DOWN_MOD_PRESSED;
+        }
+        break;
+    case CAPS_DOWN_MAYBE_CHANGE:
+        if (equal(input, &capslock_up)) {
+            output[k++] = *state->key_down;
+            output[k++] = state->last_input;
+            output[k++] = *state->key_up;
+            state->fsm = CAPS_UP;
+            break;
+        } else if ((equal(input, &lctrl_up) && equal(&state->last_input, &lctrl_down)) ||
+                   (equal(input, &rctrl_up) && equal(&state->last_input, &rctrl_down))) {
+            state->key_down = &lctrl_down;
+            state->key_up = &lctrl_up;
+            output[k++] = *state->key_down;
+        } else if ((equal(input, &ralt_up) && equal(&state->last_input, &ralt_down)) ||
+                   (equal(input, &lalt_up) && equal(&state->last_input, &lalt_down))) {
+            state->key_down = &ralt_down;
+            state->key_up = &ralt_up;
+            output[k++] = *state->key_down;
+        } else {
+            output[k++] = *state->key_down;
+            output[k++] = state->last_input;
+            output[k++] = *input;
+        }
+        state->fsm = CAPS_DOWN_MOD_PRESSED;
+        break;
+    case CAPS_DOWN_MOD_PRESSED:
+        if (equal(input, &capslock_up)) {
+            output[k++] = *state->key_up;
+            state->fsm = CAPS_UP;
+        } else if (equal(input, &lctrl_down) || equal(input, &rctrl_down) ||
+                   equal(input, &lalt_down)  || equal(input, &ralt_down)) {
+            output[k++] = *state->key_up;
+            state->last_input = *input;
+            state->fsm = CAPS_DOWN_MAYBE_CHANGE;
+        } else if (input->code != state->key_up->code) {
+            output[k++] = *input;
+        }
+        break;
     }
 
-    if (equal(input, &capslock_down)) {
-        capslock_is_down = 1;
-        return 0;
-    }
-
-    if (equal(input, &esc_down))
-        output[0] = capslock_down;
-    else if (equal(input, &esc_up))
-        output[0] = capslock_up;
-    else
-        output[0] = *input;
-
-    return 1;
+    return k;
 }
 
 int eventmap_loop(const char *devnode) {
     int result = 0;
-    int fd     = open(devnode, O_RDONLY);
+    int fd = open(devnode, O_RDONLY);
     if (fd < 0)
         return 0;
 
@@ -116,14 +142,15 @@ int eventmap_loop(const char *devnode) {
 
     struct libevdev_uinput *udev;
     if (libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED,
-                                           &udev) < 0)
+                &udev) < 0)
         goto teardown_grab;
 
+    struct state_s state = {.fsm = CAPS_UP, .key_up = &lctrl_up,
+        .key_down = &lctrl_down, .last_input = {} };
     for (;;) {
         struct input_event input;
-        int                rc = libevdev_next_event(
-            dev, LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING,
-            &input);
+        int rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL |
+                LIBEVDEV_READ_FLAG_BLOCKING, &input);
 
         while (rc == LIBEVDEV_READ_STATUS_SYNC)
             rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &input);
@@ -134,10 +161,10 @@ int eventmap_loop(const char *devnode) {
         if (rc != LIBEVDEV_READ_STATUS_SUCCESS)
             break;
 
-        struct input_event output[2];
-        for (int i = 0, k = eventmap(&input, output); i < k; ++i)
-            if (libevdev_uinput_write_event(
-                    udev, output[i].type, output[i].code, output[i].value) < 0)
+        struct input_event output[3];
+        for (int i = 0, k = eventmap(&input, output, &state); i < k; ++i)
+            if (libevdev_uinput_write_event(udev, output[i].type, output[i].code,
+                    output[i].value) < 0)
                 goto teardown_udev;
     }
 
@@ -157,16 +184,16 @@ teardown_fd:
 
 void eventmap_exec(const char *self_path, const char *devnode) {
     switch (fork()) {
-        case -1:
-            fprintf(stderr, "Fork failed on %s %s (%s)\n", self_path, devnode,
-                    strerror(errno));
-            break;
-        case 0: {
-            char *command[] = {(char *)self_path, (char *)devnode, NULL};
-            execvp(command[0], command);
-            fprintf(stderr, "Exec failed on %s %s (%s)\n", self_path, devnode,
-                    strerror(errno));
-        } break;
+    case -1:
+        fprintf(stderr, "Fork failed on %s %s (%s)\n", self_path, devnode,
+                strerror(errno));
+        break;
+    case 0: {
+        char *command[] = {(char *)self_path, (char *)devnode, NULL};
+        execvp(command[0], command);
+        fprintf(stderr, "Exec failed on %s %s (%s)\n", self_path, devnode,
+                strerror(errno));
+    } break;
     }
 }
 
@@ -185,8 +212,8 @@ int should_grab(struct udev_device *device, int initial_scan) {
             return 0;
     }
 
-    const char  input_prefix[] = "/dev/input/event";
-    const char *devnode        = udev_device_get_devnode(device);
+    const char input_prefix[] = "/dev/input/event";
+    const char *devnode = udev_device_get_devnode(device);
     if (!devnode || strncmp(devnode, input_prefix, sizeof(input_prefix) - 1))
         return 0;
 
@@ -234,7 +261,7 @@ int main(int argc, const char *argv[]) {
 
     struct sigaction sa;
     memset(&sa, 0, sizeof sa);
-    sa.sa_flags   = SA_NOCLDSTOP;
+    sa.sa_flags = SA_NOCLDSTOP;
     sa.sa_handler = &kill_zombies;
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
         perror("Couldn't summon zombie killer");
